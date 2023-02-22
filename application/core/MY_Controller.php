7b086cfaeee217a6ceec3f8103ce6b1cdf3d933c
DediRW <dedi@rumahweb.co.id>
2023-02-22 11:14:27 +0700

    first

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class MY_Controller extends RestController{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+}
